2712aa70376922648292f5c9a281ee32a694434f
Manueljcm04 <159296137+Manueljcm04@users.noreply.github.com>
2024-02-25 19:51:26 +0000

    VANILLAJS-HELLO

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+  ];
+  let theDog = who[0] + " " + action[0] + " " + what[0] + " " + when[0];
+  let myGrandma = who[1] + " " + action[2] + " " + what[1] + " " + when[3];
+  let hisTurtle = who[2] + " " + action[1] + " " + what[2] + " " + when[2];
+  let myBird = who[3] + " " + action[3] + " " + what[2] + " " + when[4];
+
+  let who1 = "The dog";
+
+  if (who1 == "The dog") {
+    document.getElementById("excuse").innerHTML = theDog;
+  } else {
+    document.getElementById("excuse").innerHTML = "No hay opcion";
+  }
+
+  let who2 = "My grandma";
+
+  if (who2 == "My grandma") {
+    document.getElementById("excuse1").innerHTML = myGrandma;
+  } else {
+    document.getElementById("excuse1").innerHTML = "No hay opcion";
+  }
+
+  let who3 = "His turtle";
+
+  if (who3 == "His turtle") {
+    document.getElementById("excuse2").innerHTML = hisTurtle;
+  } else {
+    document.getElementById("excuse2").innerHTML = myBird;
+  }
+
+  let who4 = "My bird";
+
+  if (who4 != "My bird") {
+    document.getElementById("excuse3").innerHTML = hisTurtle;
+  } else {
+    document.getElementById("excuse3").innerHTML = myBird;
+  }
 };
